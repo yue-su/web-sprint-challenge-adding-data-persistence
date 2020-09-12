731cb64061231018aa51c896b05459bd39ff111a
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-09-12 02:13:30 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -21,14 +21,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: {
-      host: "ec2-34-194-198-176.compute-1.amazonaws.com",
-      port: 5432,
-      database: "d38tuu4fujm3m5",
-      user: "bxgqpeinqpvamh",
-      password:
-        "709faff35c276ea3db9a68f35cbba45e60184d3472f865292df3e9cfec9a0137",
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./data/migrations",
     },
